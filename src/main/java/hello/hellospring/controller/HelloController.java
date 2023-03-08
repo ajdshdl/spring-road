d6836a6fc5f6782a9e065c1a2cfe52961e4a52cf
kdh <ajdshdl@naver.com>
2023-03-08 11:06:35 +0900

    변경

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,23 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "Hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name",required = true)String name, Model model){
+        //localhost:8080/hello-mvc?name=spring
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+}
