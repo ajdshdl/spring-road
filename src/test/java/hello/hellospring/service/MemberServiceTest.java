2ee306d50dc1ed38aa44d3714b71019aced8f2ce
kdh <ajdshdl@naver.com>
2023-03-08 14:30:10 +0900

    add service test

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -52,10 +52,10 @@ class MemberServiceTest {
         Long saveId1 = memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
 
         //then
-
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
     }
     @Test
